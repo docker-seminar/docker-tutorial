0dcbb4761933fe34207307ac2e91f9e053051b0d
kimseungbin <luxorienskim@gmail.com>
2025-03-22 18:34:12 +0900

    feat(Server): Add Swagger Module
    #22

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,28 @@
 // noinspection JSIgnoredPromiseFromCall
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+
+	const documentBuilderConfig = new DocumentBuilder()
+		.setTitle('Docker Tutorial')
+		.setDescription('The Docker Tutorial API description.')
+		.setContact('SeungBin Kim', 'https://github.com/docker-seminar/docker-tutorial', 'luxorienskim@gmail.com')
+		.setOpenAPIVersion('3.1.1')
+		// Todo: Use environment variable to set version dynamically
+		.setVersion('0.0.0')
+		.build()
+
+	const documentFactory = () =>
+		SwaggerModule.createDocument(app, documentBuilderConfig, {
+			autoTagControllers: true,
+		})
+	SwaggerModule.setup('api', app, documentFactory)
+
 	await app.listen(process.env.PORT ?? 3000)
 }
 
