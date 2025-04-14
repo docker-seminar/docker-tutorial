786e1650c15b2f9b82c640cad941b18d719eab8c
SeungBin Kim <luxorienskim@gmail.com>
2025-04-14 16:19:20 +0900

    test(Unit): Refactor ConfigModule tests under AppModule describe block
    #44

--- server/src/app.module.spec.ts
+++ server/src/app.module.spec.ts
@@ -2,16 +2,18 @@ import { Test } from '@nestjs/testing'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 
-describe('AppModule with ConfigModule', () => {
-	it('should load NODE_ENV from process.env', async () => {
-		process.env.NODE_ENV = 'test'
+describe('AppModule', () => {
+	describe('ConfigModule', () => {
+		it('should load NODE_ENV from process.env', async () => {
+			process.env.NODE_ENV = 'test'
 
-		const moduleRef = await Test.createTestingModule({
-			imports: [AppModule],
-		}).compile()
+			const moduleRef = await Test.createTestingModule({
+				imports: [AppModule],
+			}).compile()
 
-		const configService = moduleRef.get(ConfigService)
+			const configService = moduleRef.get(ConfigService)
 
-		expect(configService.get('NODE_ENV')).toBe('test')
+			expect(configService.get('NODE_ENV')).toBe('test')
+		})
 	})
 })
