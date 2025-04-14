b98140d8c07df459e34c5ac9044fa7765874a6bc
SeungBin Kim <luxorienskim@gmail.com>
2025-04-14 16:15:16 +0900

    feat(Server): Verify ConfigModule loads NODE_ENV from environment
    #44

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { DockerfilesModule } from './dockerfiles/dockerfiles.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-	imports: [DockerfilesModule],
+	imports: [ConfigModule.forRoot(), DockerfilesModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
